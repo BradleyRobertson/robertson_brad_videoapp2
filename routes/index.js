2dc89db159407de3af2b6e996abb9d525008e531
tvanrys <tvanrys@fanshaweonline.ca>
2018-04-19 13:25:13 -0400

    updates to views and routes

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  console.log('get all the movies');
-  connect.query(`SELECT * FROM tbl_name m, tbl_genre g, tbl_mov_genre mg WHERE m.movie_id = mg.movie_id AND g.genre_id = mg.genre_id`, (error, rows)=> {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(rows);
-      res.render('home', {
-        defaultMovie : rows[Math.floor(Math.random() * rows.length)],
-        data : JSON.stringify(rows)
-      });
-    }
-  });
+  console.log('load splash');
+  // connect.query(`SELECT * FROM tbl_name m, tbl_genre g, tbl_mov_genre mg WHERE m.movie_id = mg.movie_id AND g.genre_id = mg.genre_id`, (error, rows)=> {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log(rows);
+  //     res.render('home', {
+  //       defaultMovie : rows[Math.floor(Math.random() * rows.length)],
+  //       data : JSON.stringify(rows)
+  //     });
+  //   }
+  // });
+  res.render('home');
 });
 
 router.get('/adults', function(req, res, next){
